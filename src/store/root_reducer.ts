ea20202302cf679a7e8061221755a53e23ed36ab
tomasloree419 <tomasloree419@gmail.com>
2024-01-16 19:40:11 -0500

    second commit

--- src/store/root_reducer.ts
+++ src/store/root_reducer.ts
@@ -2,5 +2,5 @@ import { combineReducers, Reducer } from "redux";
 import postsReducer from "./posts/postsSlice";
 
 export const rootReducer: Reducer = combineReducers({
-    posts: postsReducer
-})
+  posts: postsReducer,
+});
